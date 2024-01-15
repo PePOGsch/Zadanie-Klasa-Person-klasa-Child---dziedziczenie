7978cfa3fdf8594aa7e6e82719a83aba53a32e77
PePOGsch <147649525+PePOGsch@users.noreply.github.com>
2024-01-15 22:12:22 +0100

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+
+public class Person
+{
+    private string firstName;
+    public string FirstName
+    { 
+        get=>firstName;
+        set
+        {
+            value = value.Replace(" ", "");
+            Regex regex = new Regex("^[a-zA-Z ąćęłńóśźżĄĆĘŁŃÓŚŹŻ-]+$");
+
+            if (value == null || value == "" || value.Length <2 || value.Split('-').Length > 2 || !(regex.IsMatch(value)))
+                throw new ArgumentException("Wrong name!");
+
+            firstName = char.ToUpper(value[0]) + value.ToLower().Substring(1);
+        }
+    }
+
+    private string familyName;
+    public string FamilyName 
+    { 
+        get=>familyName; 
+        set
+        {
+            value = value.Replace(" ", "");
+
+            Regex regex = new Regex("^[a-zA-Z ąćęłńóśźżĄĆĘŁŃÓŚŹŻ-]+$");
+
+            if (value == null || value == "" || value.Length < 2 || value.Split('-').Length > 2 || !(regex.IsMatch(value)))
+                throw new ArgumentException("Wrong name!");
+
+            familyName = char.ToUpper(value[0]) + value.ToLower().Substring(1);
+        }
+    }
+    private int age;
+    public int Age 
+    { 
+        get=>age;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Age must be positive!");
+            if (this is Child && (value > 15))
+                throw new ArgumentException("Child’s age must be less than 15!");
+
+            age = value;
+        }
+
+    }
+    public Person(string firstName, string familyName, int age)
+    {
+        FirstName = firstName;
+        FamilyName = familyName;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {FamilyName} ({Age})";
+    }
+
+    public void modifyFirstName(string newfirstName)
+    {
+        FirstName = newfirstName;
+    }
+
+    public void modifyFamilyName(string newfamilyName)
+    {
+        FamilyName = newfamilyName;
+    }
+
+    public void modifyAge(int newAge)
+    {
+        Age = newAge;
+    }
+}
+
+public class Child : Person
+{
+
+    private Person mother;
+    public Person Mother
+    {
+        get { return mother; }
+    }
+
+    private Person father;
+    public Person Father
+    {
+        get { return father; }
+    }
+
+
+    public Child(string firstName, string familyName, int age, Person mother = null, Person father = null)
+        :base(firstName, familyName, age)
+    {
+        if (age > 15)
+        {
+            throw new ArgumentException("Child’s age must be less than 15!");
+        }
+
+        this.mother = mother;
+        this.father = father;
+    }
+
+
+    public override string ToString()
+    {
+        string motherInfo = (mother != null) ? $"mother: {mother.ToString()}" : "mother: No data";
+        string fatherInfo = (father != null) ? $"father: {father.ToString()}" : "father: o data";
+        return $"{base.ToString()}\n{motherInfo}\n{fatherInfo}";
+       // return $"{FirstName} {FamilyName} ({Age})\nmother: {mother.ToString()}\nfather: {father.ToString()}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+//Test1();
+//Test2();
+//Test3();
+//Test4();
+//Test5();
+//Test6();
+//Test7();
+Test8();
+
+
+/////-----------
+///
+
+void Test1()
+{
+    /* Test: poprawne tworzenie obiektu Person, dane poprawne */
+    try
+    {
+        Person p = new Person(familyName: "Molenda", firstName: "Krzysztof", age: 18);
+        Console.WriteLine(p);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+void Test2()
+{
+    /* Test: poprawne tworzenie obiektu Person, 
+   błędne imię lub nazwisko, pierwsza duża pozostałe małe */
+    try
+    {
+        Person p = new Person(familyName: "MOlenda", firstName: "krzysztof", age: 18);
+        Console.WriteLine(p);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+void Test3()
+{
+    /* Test: poprawne tworzenie obiektu Person, 
+   błędne imię lub nazwisko, niewłaściwy wiek */
+    try
+    {
+        Person p = new Person(familyName: "MOlenda", firstName: "krzysztof", age: -18);
+        Console.WriteLine(p);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+void Test4()
+{
+    /* Test: poprawne tworzenie obiektu Person,
+   błędne imię lub nazwisko, spacje przed i po */
+    try
+    {
+        Person p = new Person(familyName: "  Molenda    ", firstName: " Krzysztof ", age: 18);
+        Console.WriteLine(p);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+void Test5()
+{
+
+    /* Test: modyfikacja obiektu */
+    try
+    {
+        Person p = new Person(familyName: "Molenda", firstName: "Krzysztof", age: 18);
+        p.modifyFirstName(" Ja n");
+        p.modifyFamilyName("kolenda ");
+        p.modifyAge(35);
+        Console.WriteLine(p);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+void Test6()
+{
+    /* Test: poprawne tworzenie obiektu Person,
+   błędne imię lub nazwisko, spacje w środku */
+    try
+    {
+        Person p = new Person(familyName: "Molen  da", firstName: "Krzysztof.", age: 18);
+        Console.WriteLine(p);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+void Test7()
+{
+    /* Test: tworzenie obiektu Child */
+    try
+    {
+        Person o = new Person(familyName: "Molenda", firstName: "Krzysztof", age: 30);
+        Person m = new Person(familyName: "Molenda", firstName: "Ewa", age: 29);
+        Child d = new Child(familyName: "Molenda", firstName: "Anna", age: 10,
+                            mother: m, father: o);
+        Console.WriteLine(d);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+void Test8()
+{
+    /* Test: tworzenie obiektu Child
+   modyfikacja danych */
+    try
+    {
+        Person o = new Person(familyName: "Molenda", firstName: "Krzysztof", age: 30);
+        Person m = new Person(familyName: "Molenda", firstName: "Ewa", age: 29);
+        Child d = new Child(familyName: "Molenda", firstName: "Anna", age: 14,
+                            mother: m, father: o);
+        d.modifyFirstName("Aneta");
+        Console.WriteLine(d);
+        d.modifyFirstName("Kolenda");
+        Console.WriteLine(d);
+        d.modifyAge(18);
+        Console.WriteLine(d);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
